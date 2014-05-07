02db6beeb72f15c5e190077c39273aa9a90e950f
switu <shahparth5864@yahoo.com>
2014-05-07 14:21:25 -0400

    NEw
    NEw

---
+++ new/new/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  new
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 User1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
